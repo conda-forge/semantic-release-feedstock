cb2e60c92b739ff2430aed39265cb2ae0e7b9314
Daniel Nachun <daniel.nachun@gmail.com>
2025-07-04 10:59:54 -0700

    fix build

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,6 +5,7 @@ set -o xtrace -o nounset -o pipefail -o errexit
 # Create package archive and install globally
 npm pack --ignore-scripts
 npm install -ddd \
+    --no-bin-links \
     --global \
     --build-from-source \
     ${SRC_DIR}/${PKG_NAME}-${PKG_VERSION}.tgz
@@ -15,6 +16,13 @@ jq 'del(.packageManager)' package.json.bak > package.json
 pnpm install
 pnpm-licenses generate-disclaimer --prod --output-file=third-party-licenses.txt
 
-tee ${PREFIX}/bin/${PKG_NAME}.cmd << EOF
-call %CONDA_PREFIX%\bin\node %PREFIX%\bin\semantic-release %*
+mkdir -p ${PREFIX}/bin
+tee ${PREFIX}/bin/semantic-release << EOF
+#!/bin/sh
+exec \${CONDA_PREFIX}/lib/node_modules/semantic-release/bin/semantic-release.js "\$@"
+EOF
+chmod +x ${PREFIX}/bin/semantic-release
+
+tee ${PREFIX}/bin/semantic-release.cmd << EOF
+call %CONDA_PREFIX%\bin\node %CONDA_PREFIX%\lib\node_modules\semantic-release\bin\semantic-release.js %*
 EOF
